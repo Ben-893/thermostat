c3ab5a0c3b4b1c7b38b2b95a303cddb84ea2ea79
Benjamin <ben.smith.gordon@gmail.com>
2018-07-13 10:47:51 +0100

    A lot of user stories added after pairing with daniel

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,7 +1,70 @@
-'use Strict';
-
-describe('thermostat', function() {
+'use strict';
 
 
+describe('Thermostat', function() {
+
+  var thermostat;
+  
+
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('can increase temperature', function() {
+    thermostat.increase();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('can decrease temperature', function() {
+    thermostat.decrease();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degrees', function() {
+    expect(thermostat.MINIMUM_TEMP).toEqual(10);
+  });
+
+  it('has a power saving mode', function() {
+    thermostat.powerSavingModeOn();
+    expect(thermostat.powerSavingMode).toEqual(true);
+  });
+
+describe('PSM on', function(){
+    it('has a maximum temperature of 25 degrees when powerSavingMode is on', function() {
+      thermostat.powerSavingModeOn();
+      expect(thermostat.MAX_TEMP_PSM_ON).toEqual(25)
+    });
+
+    it('throws an error when higher than max temp', function() {
+      for(var i = 0; i < 5; i++) {
+        thermostat.increase()
+        console.log(thermostat);
+      };
+
+      expect(function() { thermostat.increase(); }).toThrow(new Error('Temperature already at maximum'));
+
+    });
+
+  });
+
+  it('has a maximum temperature of 32 degrees when powerSavingMode is off', function() {
+    thermostat.powerSavingModeOff();
+    expect(thermostat.MAX_TEMP_PSM_OFF).toEqual(32)
+  });
+
+  it('has a power saving mode', function() {
+    thermostat.powerSavingModeOff();
+    expect(thermostat.powerSavingMode).toEqual(false);
+  });
+
+  it('PSM on by default', function() {
+    expect(thermostat.powerSavingMode).toEqual(true);
+  });
 
 });
+
---
+++ src/thermostat.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20;
+  this.MINIMUM_TEMP = 10;
+  this.MAX_TEMP_PSM_ON = 25;
+  this.MAX_TEMP_PSM_OFF = 32;
+  this.powerSavingMode = true;
+
+
+};
+
+Thermostat.prototype.increase = function() {
+
+  if(this.temperature + 1 > this.maxTemp()){
+    throw new Error('Temperature already at maximum')
+  } else {
+  this.temperature += 1
+  }
+  // console.log(this.maxTemp())
+};
+
+
+Thermostat.prototype.decrease = function() {
+  this.temperature -= 1
+};
+
+Thermostat.prototype.powerSavingModeOn = function() {
+  this.powerSavingMode = true
+};
+
+Thermostat.prototype.powerSavingModeOff = function() {
+  this.powerSavingMode = false
+};
+
+Thermostat.prototype.maxTemp = function() {
+  if (this.powerSavingMode === true) {
+   return this.MAX_TEMP_PSM_ON
+  } else {
+   return this.MAX_TEMP_PSM_OFF 
+  };
+
+
+};
